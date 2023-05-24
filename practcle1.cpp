403edbb87fd4f9b603f8ecba79d9f49492bf2177
Sakshi3130 <110780856+Sakshi3130@users.noreply.github.com>
2023-05-24 08:35:51 +0530

    Add files via upload

--- practcle1.cpp
+++ practcle1.cpp
@@ -1,12 +1,13 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 class node
 {
     public:
-    node *temp,*next, *down;
+    node *temp,*next, *down,*t4;
     node *root=NULL;
     int flag;
+    char ch[20];
     char name[20];
     node *create()
     {
@@ -64,6 +65,46 @@ class node
                 }
         }
     }
+    void insertS(){
+        if(root==NULL){
+            cout<<"Cannot add sections"<<endl;
+        }
+        else{
+            int n;
+            cout<<"Enter chapter name to which you want to add section."<<endl;
+            cin>>ch;
+            temp=root;
+            if(temp->flag==0){
+                cout<<"there are no chapters in the book"<<endl;
+
+            }
+            else{
+                temp = temp->down;
+                while(temp!=NULL){
+                    if(!strcmp(ch,temp->name)){
+                        cout<<"enter number of sections you want to add: "<<endl;
+                        cin>>n;
+                        for(int i=0;i<n;i++){
+                            node* t3 = create();
+                            if(temp->flag==0){
+                                temp->down = t3;
+                                temp->flag=1;
+                                t4=temp->down;
+                            }
+                            else{
+                                while(t4->next!=NULL){
+                                    t4=t4->next;
+                                }
+                                t4->next = t3;
+                            }
+                        }
+                        break;
+                    }
+                    temp = temp->next;
+                }
+            }
+        }
+    }
 
     void display()
     {
@@ -74,26 +115,33 @@ class node
         }
         else
         {
-            
+            temp = root;
             cout<<endl<<endl<<"name of the book: "<<endl;
             cout<<root->name<<endl<<endl;
-            if(root->down==NULL)
+            if(root->flag==0)
             {
                     cout<<"No chapters to display."<<endl;
             }
             else
             {
-                
-                
                 cout<<"chapters: "<<endl;
-                temp=root->down;
+                temp=temp->down;
                 while(temp!=NULL)
                 {
-                    cout<<i+1<<") "<<temp->name<<"   ";
-                    temp=temp->next;
+                    cout<<i+1<<") "<<temp->name<<endl;
+                    t4 = temp;
+                    if(t4->flag==1){
+                        cout<<"sections of "<<i+1<<" chapter"<<endl;
+                        t4=t4->down;
+                        while(t4!=NULL){
+                            cout<<t4->name<<endl;
+                            t4=t4->next;
+                        }
+                    }
+                    temp = temp->next;
                     i++;
                 }
-                
+                temp=temp->down;
             }
         }
     }
@@ -108,6 +156,7 @@ int main()
         node ob;
         ob.insertB();
         ob.insertC();
+        ob.insertS();
         cout<<endl;
         ob.display();
         return 0;
